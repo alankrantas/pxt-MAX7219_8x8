5f9b743998d17213197f8534b680542bc261ee80
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2019-03-28 21:03:29 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,24 @@
-// tests go here; this will not be compiled when this package is used as a library
+MAX7219_Matrix.setup(
+4,
+DigitalPin.P16,
+DigitalPin.P15,
+DigitalPin.P14,
+DigitalPin.P13
+)
+basic.forever(function () {
+    MAX7219_Matrix.scrollText(
+    "Hello world!",
+    50,
+    50
+    )
+    for (let index = 0; index <= 23; index++) {
+        MAX7219_Matrix.displayCustomCharacter(
+        MAX7219_Matrix.getCustomCharacterArray(
+        "B00100000,B01000000,B10000110,B10000000,B10000000,B10000110,B01000000,B00100000"
+        ),
+        index,
+        true
+        )
+        basic.pause(50)
+    }
+})
